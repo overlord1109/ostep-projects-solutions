0660bc575e23a724285c49d5167d1b5d8d0944ee
overlord1109 <mandarmahajan1996@gmail.com>
2020-08-15 18:51:03 +0530

    Completed wcat part of p1a; Added .gitignore

---
+++ initial-utilities/wcat/wcat.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char** argv) {
+	if(argc == 1)
+		exit(0);
+	for(int i = 1; i<argc; i++) {
+		FILE * fd = fopen(argv[i], "r");
+		if(fd == NULL) {
+			printf("wcat: cannot open file\n");
+			exit(1);
+		}
+		char str[512];
+		while(fgets(str, sizeof(str), fd) != NULL)
+			printf("%s", str);
+	}
+	exit(0);
+
+}
